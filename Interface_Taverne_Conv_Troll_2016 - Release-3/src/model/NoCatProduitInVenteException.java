7893798f2f6fa90df745bfa0c35a92d81c245692
TheDinoSlayer <theyoshi@gmail.com>
2016-05-27 14:46:57 +0200

    Modification pour respecter le Modèle MVC

--- Interface_Taverne_Conv_Troll_2016 - Release-3/src/model/NoCatProduitInVenteException.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-
-public class NoCatProduitInVenteException extends Exception{
-    public NoCatProduitInVenteException(){
-        super("This product has no categorie -> sale aborted");
-    }
-}
